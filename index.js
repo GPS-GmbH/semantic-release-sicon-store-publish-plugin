8bfbdf6a23a8c54d28897a05e695c6a3b2771d1f
Martin Muzatko <martin@happy-css.com>
2020-01-16 16:58:36 +0100

    extact version info as variable

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { getApp } = require('sicon-app-store-deploy')
 const publish = async ({ app, maturity = 'stable', dockerTag, name, changelog }, { nextRelease, branch }) => {
-    const appStore = getApp(process.env.APPSTORE_LOGIN_APP || app)
-    return appStore.publishVersion({
+    const version = {
         maturity,
         name: name || nextRelease.name,
         dockerTag: dockerTag || branch.name,
         changelog: changelog || nextRelease.notes,
-    })
+    }
+    const appStore = getApp(process.env.APPSTORE_LOGIN_APP || app)
+    return appStore.publishVersion(version)
 }
 module.exports = {
     publish,
